642ff46a46205bed07f90a9f4b9a8318e4f057a3
twistby <pref@outlook.com>
2021-02-16 14:19:43 +0100

    end step 3

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,8 @@
+"""cli.py module."""
 import prompt
 
 
 def welcome_user():
+    """Ask a user name."""
     name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
+    print('Hello, {user_name}!'.format(user_name=name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python3
+"""Brain game module."""
+from brain_games import cli
 
-import brain_games.cli
 
 def main():
+    """Start of programm."""
     print('Welcome to the Brain Games!')
-    brain_games.cli.welcome_user()
+    cli.welcome_user()
+
 
 if __name__ == '__main__':
+
     main()
